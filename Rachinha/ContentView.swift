3599b54fd9bebf242ef9470a753f213ce7e45b47
AlvaroNascimento94 <alvarosum@hotmail.com>
2024-08-19 23:13:42 -0300

    Initial Commit

---
+++ Rachinha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rachinha
+//
+//  Created by Alvaro Filho on 19/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rachinha/RachinhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RachinhaApp.swift
+//  Rachinha
+//
+//  Created by Alvaro Filho on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RachinhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
